b4d255639a5b05379477ffe096614431eeddc48e
Will Darley <16darlew@fulford.york.sch.uk>
2022-01-05 10:01:45 +0000

    added instructions

--- Fruit machine/Form1.cs
+++ Fruit machine/Form1.cs
@@ -12,6 +12,10 @@ namespace Fruit_machine
 {
     public partial class Form1 : Form
     {
+        //  Write a program to simulate a Fruit Machine that displays three symbols at random from Cherry, Bell, Lemon, Orange, Star, Skull.
+
+        // The player starts with Â£1 credit, with each go costing 20p.If the Fruit Machine rolls two of the same symbol, the user wins 50p.The player wins £1 for three of the same and £5 for 3 Bells.
+        // The player loses £1 if two skulls are rolled and all of his/her money if three skulls are rolled.The player can choose to quit with the winnings after each roll or keep playing until there is no money left.
         public Form1()
         {
             InitializeComponent();
